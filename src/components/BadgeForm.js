46ca313e89dcf6c5345c3570ec9614964a81f454
Turiano Romero <romero.guzman18@gmail.com>
2019-07-19 13:02:33 -0500

    enlazando eventos :sunglasses:

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
+  _handleChange = (e) => {
+    console.log({ 
+      value: e.target.value,
+      name: e.target.name
+     })
+  }
+  _handleClick = () => {
+    console.warn('Click in the button')
+  }
   render() {
     return (
       <div>
         <h1>New Attendand</h1>
-        <from>
+        <form>
           <div className='form-group'>
             <label>First Nane</label>
-            <input className='form-control' type='text' name='firstName'/>
+            <input onChange={this._handleChange} className='form-control' type='text' name='firstName'/>
           </div>
-          <button className='btn btn-primary'>Save</button>
-        </from>
+          <button type='button' onClick={this._handleClick} className='btn btn-primary'>Save</button>
+        </form>
       </div>
     );
   }
